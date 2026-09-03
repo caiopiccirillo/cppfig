ae38551b55e246aec6db1547848855f7790e7e02
Caio Piccirillo <caiopiccirillo@gmail.com>
2026-09-03 14:52:21 -0300

    fix: Use the platform temp directory and a seeded generator in test helpers

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1351,7 +1351,7 @@ TEST(StatusOrTest, RejectsConstructionFromOkStatus)
 TEST(ConfigurationTestFixtureTest, CreateTempFilePathDefault)
 {
     auto path = testing::ConfigurationTestFixture::CreateTempFilePath();
-    EXPECT_NE(path.find("/tmp/"), std::string::npos);
+    EXPECT_TRUE(path.starts_with(std::filesystem::temp_directory_path().string()));
     EXPECT_NE(path.find("test_config"), std::string::npos);
     EXPECT_NE(path.find(".conf"), std::string::npos);
 }
@@ -1359,7 +1359,7 @@ TEST(ConfigurationTestFixtureTest, CreateTempFilePathDefault)
 TEST(ConfigurationTestFixtureTest, CreateTempFilePathWithPrefix)
 {
     auto path = testing::ConfigurationTestFixture::CreateTempFilePath("my_prefix");
-    EXPECT_NE(path.find("/tmp/"), std::string::npos);
+    EXPECT_TRUE(path.starts_with(std::filesystem::temp_directory_path().string()));
     EXPECT_NE(path.find("my_prefix"), std::string::npos);
 }
 
